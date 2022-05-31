b7f5c4934db6cb5df07d95c49f6a5ddaf2097b18
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-05-31 19:20:35 -0300

    O Programa realiza a soma de dois números determinados pelo programador e armazena em um determinado local na memória.
    O Programa realiza a soma de dois números e armazena em um determinado local na memória. Porém, tudo isso é feito pelo usuário manualmente.

--- homework1/sumof2binarynumbers.asm
+++ homework1/sumof2binarynumbers.asm
@@ -1,6 +1,13 @@
-LXI H, 2050H ; take the first input
-MOV A, M ;
-INX H ; incrementa para 2051
-ADD M ; A = A+M
-STA 2052H ; 07H+05H = 0CH (12)
-HLT
+; Programa realiza a soma de dois números e armazena em um determinado local na memória. Porém, tudo isso é feito pelo usuário manualmente
+
+.org 2000H ; Inicia em 2000
+
+MVI A, 02H ; Primeiro número
+MVI B, 03H ; Segundo número
+ADD B ; A+B and will be stored in A
+STA 2052H ; Store acumulated content(sta)
+HLT ; Stop the programm execution - Para o programa
+
+; Nesse caso:
+; 02H + 03H = 05H
+; E esse valor é armazenado em 2052
