033516a97757ffe973600bce8e9121c9ca14b3da
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-05-31 19:48:17 -0300

    O programa faz a soma de dois números mas ainda não indica a flag de carry

--- homework1/sumof2binarynumbers02.asm
+++ homework1/sumof2binarynumbers02.asm
@@ -4,7 +4,7 @@
 
 LXI H, 2050H ; Take the first input, no endereço 2050
 MOV A, M ; the content of the memory is copied in the accumullator
-INX H ; incrementa para o endereço 2051H
+INX H ; incrementa, no caso para o endereço 2051H
 ADD M ; A => A+M
 STA 2052H ; por ex: 07H+05H = 0CH (12)
 HLT
