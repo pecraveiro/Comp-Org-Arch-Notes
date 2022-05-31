5da9e128ad75af4fcf0dfe2137ce09fd0384176f
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-05-31 20:15:34 -0300

    O programa faz a adição de dois números com a adição de carry se necessário

---
+++ homework1/sumof2binarynumbers.asm
@@ -0,0 +1,22 @@
+; O programa faz a adição de dois números com a adição de carry se necessário
+
+; Exemplo:
+; F1 (hexa) -> 11110001 (binária)
+; 15 (hexa) -> 00010101 (binária)
+; Somando:     --------
+;          (1) 00000110 (CARRY gerado)
+
+; Iniciando o programa
+.org 2000H
+
+LXI H, 2050H ; First Input - Primeiro input nesse endereço de memória
+MOV A, M ; A <- "A", now contains the input
+INX H ; Incrementa o endereço de memória para 2051H
+MVI B, 00H
+ADD M
+JNC loop ; JNC - Jump if carry is not generate / Jump if no carry
+INR B ; (B <- B+1 <- 01H)
+loop: STA 2053H
+MOV A,B
+STA 2052H
+HLT
