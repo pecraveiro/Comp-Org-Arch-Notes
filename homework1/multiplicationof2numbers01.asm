54b009dc798d8180ea19519057d38c06dc687fc5
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-06-01 23:23:08 -0300

    O Programa realiza a multiplicação de dois números e armazena em um determinado local na memória.
    Fiz a atualização dos comentários

--- homework1/multiplicationof2numbers01.asm
+++ homework1/multiplicationof2numbers01.asm
@@ -11,14 +11,14 @@ LXI H, 2050H  ; Primeiro input e armazenando no endereço de memória 2050H
 MOV B, M ; B será o "fator ou multiplicando"
 MVI A, 00H ; por segurança iniciamos com 0
 MVI D, 00H ; this registrator will register all the carries 
-INX H
+INX H ; Incremento de um o conteúdo do par de registradores
 MOV C, M ; C será o "fator ou multiplicador"
-loop2:ADD B
-JNC loop
-INR D
-loop: DCR C
-JNZ loop2
-STA 2052H
-MOV A,D
+loop2:ADD B ; Adiciona conteúdo do registro ao acumulador
+JNC loop ; Salta para endereço se o flag carry não estiver setado
+INR D ; Incrementa de um o conteúdo do registrador
+loop: DCR C ; Decrementa de um conteúdo do registrador
+JNZ loop2 ; Salta para endereço se o flag zero não estiver setado
+STA 2052H ; Armazena em endereço o conteúdo do acumulador
+MOV A,D ; Move conteúdo de D para A
 STA 2053H ; Store the carry
-HLT
+HLT ; Para o microprocessador
