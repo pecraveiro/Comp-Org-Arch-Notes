cb7ff945be4b32e339ab97fab2182341c7f1728c
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-07-05 13:33:04 -0300

    Update findingthehighervalue.asm

--- homework2/findingthehighervalue.asm
+++ homework2/findingthehighervalue.asm
@@ -7,11 +7,11 @@ MOV C, M
 INX H
 DCR C
 MOV A, M
-skip INX H
+skip: INX H
 CMP M
 JNC loop
 MOV A, M
-loop DCR C
+loop: DCR C
 JNZ skip
 STA 400AH ; stores the higher valor, armazena o maior valor
 HLT ; Finaliza o programa
