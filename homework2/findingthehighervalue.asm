55e57b560e863d8fc0c73bc41a5a058fa923c81f
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-07-07 20:47:39 -0300

    Update findingthehighervalue.asm

--- homework2/findingthehighervalue.asm
+++ homework2/findingthehighervalue.asm
@@ -2,15 +2,15 @@
 
 .org 2000h
 
-LXI H, 4000H
-MOV C, M
-INX H
-DCR C
-MOV A, M
-skip: INX H
-CMP M
-JNC loop
-MOV A, M
+LXI H, 4000H ; input the first number in the array
+MOV C, M ; movendo para o registrador C
+INX H ; input the first data
+DCR C ; vamos colocar um novo dado
+MOV A, M ; colocamos o primeiro dado no acumulador
+skip: INX H ; input the second data
+CMP M ; comparar o acumulador com a memória
+JNC loop ; se um carry for gerado, significa que o acumulador contém o maior número
+MOV A, M ; movemos o conteúdo da memória para o acumulador
 loop: DCR C
 JNZ skip
 STA 400AH ; stores the higher valor, armazena o maior valor
